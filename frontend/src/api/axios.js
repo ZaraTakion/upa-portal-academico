563e8ea54e6dce8a2d2a2c23e2327ed10cfdf051
Rodrigo(Zara) <rodzmaciel21@gmail.com>
2026-05-25 16:41:30 -0300

    deploy: configura frontend para vercel

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
-  timeout: 10000,
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
 });
 
 api.interceptors.request.use((config) => {
@@ -14,19 +13,5 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("accessToken");
-      localStorage.removeItem("refreshToken");
-      localStorage.removeItem("currentUser");
-      window.location.href = "/";
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default api;
